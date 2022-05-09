507f44786106b20e663556f61aeeac8b4ea7d8d0
nadolf <nadolftagoe1@gmail.com>
2022-05-09 02:58:15 -0400

    First Level Completed

--- src/main/java/com/example/guess_brand_logo/HelloApplication.java
+++ src/main/java/com/example/guess_brand_logo/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.guess_brand_logo;
 import javafx.application.Application;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -13,6 +15,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -29,7 +32,10 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
-    private Parent Structure() throws FileNotFoundException {
+    public Parent Structure() throws FileNotFoundException {
+        Label title = new Label("Level 1");
+        title.setFont(new Font("Times New Roman", 25.0));
+        //
         var textbox = new CheckGuess(input -> input.matches("Pepsi") || input.matches("pepsi"));
         textbox.setText("Enter Guess");
         textbox.setFocusTraversable(false);
@@ -38,9 +44,15 @@ public class HelloApplication extends Application {
         textbox.setMaxHeight(35.0);
         //
         var button = new Button("Next Level ->");
-        button.disableProperty().bind(textbox.isCorrect.not());
-        Label title = new Label("Level 1");
-        title.setFont(new Font("Times New Roman", 25.0));
+        button.disableProperty().bind(textbox.ifCorrect.not());
+        button.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                title.setTextFill(Color.GREEN);
+                title.setText("Correct!");
+            }
+        });
         //Logo
         Image logo1 = new Image(new FileInputStream("C:\\Users\\Nadolf\\Projects\\Guess_Brand_Logo\\src\\main\\java\\com\\example\\guess_brand_logo\\pepsi-logo-symbol.png"));
         ImageView imageView = new ImageView(logo1);
@@ -57,16 +69,16 @@ public class HelloApplication extends Application {
 
     private static class CheckGuess extends TextField {
         private final Predicate<String> answer;
-        private BooleanProperty isCorrect = new SimpleBooleanProperty();
+        private BooleanProperty ifCorrect = new SimpleBooleanProperty();
 
         CheckGuess(Predicate<String> answer) {
             this.answer = answer;
 
             textProperty().addListener((o, oldValue, newText) -> {
-                isCorrect.set(answer.test(newText));
+                ifCorrect.set(answer.test(newText));
             });
 
-            isCorrect.set(answer.test(""));
+            ifCorrect.set(answer.test(""));
         }
     }
 
